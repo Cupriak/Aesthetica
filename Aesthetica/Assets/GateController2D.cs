5fd7e47f6415aa7fc69cc660e2125f9955c1a4e6
Mateusz Cupriak <mcupriak95@gmail.com>
2019-12-12 03:12:29 +0100

    Gate opening and closing mechanics added

---
+++ Aesthetica/Assets/GateController2D.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GateController2D : MonoBehaviour
+{
+    //TRIGGER ATTRIBUTES
+    public bool IsTriggered { get; set; }
+
+    private Animator animator;
+    private SpriteRenderer spriteRenderer;
+
+    [SerializeField] private GameObject doorTrigger;
+    [SerializeField] private GameObject doorSide;
+    [SerializeField] private Collider2D doorTriggerCollider;
+    [SerializeField] private LayerMask whatCanOpenDoor;
+    [SerializeField] private Timer doorTimer;
+
+    private bool doorOpen;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void Animate()
+    {
+        if (doorOpen)
+        {
+            animator.Play("GateOpen");
+        }
+        else
+        {
+            animator.Play("GateClosed");
+        }
+    }
+
+    private void Update()
+    {
+        Animate();
+        DoorOpeningDetection();
+        DoorClosingDetection();
+    }
+
+    private void DoorOpeningDetection()
+    {
+        if (Physics2D.IsTouchingLayers(doorTriggerCollider, whatCanOpenDoor))
+        {
+            doorTrigger.SetActive(false);
+            doorSide.SetActive(false);
+            doorTimer.StartTimer(3f);
+            doorOpen = true;
+        }
+    }
+
+    private void DoorClosingDetection()
+    {
+        if (doorTimer.timeElapsed)
+        {
+            doorTrigger.SetActive(true);
+            doorSide.SetActive(true);
+            doorOpen = false;
+        }
+    }
+}
---
+++ Aesthetica/Assets/GateTriggerController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GateTriggerController : MonoBehaviour
+{
+    public GateController2D gate;
+
+    [SerializeField] private LayerMask whatCanTrigger;
+
+    [SerializeField] private Timer gateTimer;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (LayerMaskHelper.IsLayerInLayerMask(collision.gameObject.layer, whatCanTrigger))
+        {
+            gate.IsTriggered = true;
+            gateTimer.StartTimer(0.5f);
+        }
+    }
+}
