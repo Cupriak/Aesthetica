415116aa20d5237bc42827326109bea99f12781d
Mateusz Cupriak <mcupriak95@gmail.com>
2019-12-13 18:48:20 +0100

    improved camera controller

--- Aesthetica/Assets/Scripts/General/CameraController.cs
+++ Aesthetica/Assets/Scripts/General/CameraController.cs
@@ -5,13 +5,21 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     [SerializeField] private Transform playerTransform;
-    [SerializeField] private float smoothSpeed;
+    [Range(0, 1)][SerializeField] private float smoothSpeed;
     [SerializeField] private Vector3 offset;
+
+    private Camera mainCamera;
+    [Range(1, 10)][SerializeField] private float cameraZoom;
+    private float defaultZoom = 2.424129f;
+
+    [SerializeField] private bool allowCameraZoom;
 
     [HideInInspector] public bool isStatic;
 
     private void Awake()
     {
+        mainCamera = GetComponent<Camera>();
+        mainCamera.orthographicSize = defaultZoom;
         transform.position = playerTransform.position;
     }
 
@@ -24,7 +32,16 @@ public class CameraController : MonoBehaviour
 
     private void FixedUpdate()
     {
-        if(!isStatic)
+        if(allowCameraZoom)
+        {
+            mainCamera.orthographicSize = cameraZoom;
+        }
+        else
+        {
+            mainCamera.orthographicSize = defaultZoom;
+        }
+
+        if (!isStatic)
         {
             if(playerTransform != null)
             {
